c7f617a9e332f168bf5800ad1866a178f75f44d7
Mussani Ruelas <mussaniruelas222@gmail.com>
2024-05-27 18:25:35 -0400

    el final1

--- ejer01/ejer1.c
+++ ejer01/ejer1.c
@@ -1,70 +1,51 @@
 #include <stdio.h>
 
-// cd ejer01
 // Función para sumar dos números
-int suma(int *a, int *b) {
-    return *a + *b;
+void suma(int *a, int *b) {
+    *a = *a + *b;
 }
 
-// Funcion para restar dos numeros
-int resta(int *a, int *b){
-    return *a - *b;
+// Función para restar dos números
+void resta(int *a, int *b) {
+    *a = *a - *b;
 }
 
 // Función para multiplicar dos números usando sumas
 int multiplicar(int *a, int *b) {
     int resultado = 0;
     for (int i = 0; i < *b; ++i) {
-        resultado = suma(&resultado, b);
+        int temp = *a;  
+        suma(&resultado, &temp);
     }
     return resultado;
 }
 
-// Divicion entera de dos numeros usando restas
-int div_entera(int *a, int *b){
+// División entera de dos números usando restas
+int div_entera(int *a, int *b) {
     int resultado = 0;
-    for (int i = *a; i > *b; i=resta(&i, b)){
-        resultado += 1;
-    }
-    return resultado;
-}
-// otra forma de ver el panorama
-// Divicion con decimales 
-/*float div_decimal(int *a, int*b){
-    float resultado = 0;
-    int residuo = *a;
-    int contador_decimales = 0;
-
-    while (residuo > *b)
-    {
-        residuo -= *b;
+    int temp = *a;
+    while (temp >= *b) {
+        resta(&temp, b);
         resultado++;
     }
-    // cosas que ya no convencen....
-    // mejor usar una serie de taylor 
-    while(contador_decimales < 8){
-        residuo *= 10;
-        float parte_decimal = 0;
-        while (residuo > *b)
-        {
-            residuo -= *b;
-            parte_decimal++;
-        }
-        resultado = resultado + parte_decimal/10;
-        contador_decimales++;
-    }
     return resultado;
-}*/
+}
 
 int main() {
     int num1 = 5;
     int num2 = 3;
+    int s = num1;
+    int r = num1;
 
-    printf("Suma: %d\n", suma(&num1, &num2));
-    printf("Resta: %d\n", resta(&num1, &num2));
+    // Realizamos las operaciones
+    suma(&s, &num2);
+    resta(&r, &num2);
+
+    // Imprimimos los resultados
+    printf("Suma: %d\n", s);
+    printf("Resta: %d\n", r);
     printf("Multiplicación: %d\n", multiplicar(&num1, &num2));
-    printf("Divicion Entera: %d\n",div_entera(&num1, &num2));
+    printf("División Entera: %d\n", div_entera(&num1, &num2));
 
     return 0;
 }
-
